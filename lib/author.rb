de58445893227b7628c9f76ee0010ce2476cdcf8
Ben Parish <parishben13@gmail.com>
2020-05-30 03:00:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,31 @@
-class Author 
-  attr_accessor :post, :name
+class Author
+  attr_accessor :name
+
   @@all = []
-  @post = []
+
   def initialize(name)
     @name = name
     @@all << self
-  end
-  def posts 
-    Posts.all.select {|a_post| a_post.author == self}
-  end
-  def add_post(post)
-    post.author = self
-  end
-
-
-  def self.post_count
-    Post.all.count
   end
 
   def self.all
     @@all
   end
-  
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
 end
