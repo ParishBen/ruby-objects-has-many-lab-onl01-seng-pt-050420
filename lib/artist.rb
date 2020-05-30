98fd853388a708e67d2b587213109cf8f44ca958
Ben Parish <parishben13@gmail.com>
2020-05-30 02:15:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,12 @@
 class Artist 
-  attr_accessor :name, :songer
+  attr_accessor :name, :song
   @songs = ["I will always love you", "Never let go, Jack", "this song", "and another hit"]
   def initialize(name)
     @name = name
     @songs = []
   end
+  def add_song(song_name)
+    
   def songs
    @songs
  end
