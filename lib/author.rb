0b298f0692ca84f74cfddf6b19a91886e9cfaad8
Ben Parish <parishben13@gmail.com>
2020-05-30 02:02:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,7 +11,7 @@ class Author
   def add_post(poster)
    poster = Post.new
    @post << poster
-   poster.Author = self
+   poster.author = self
   end  
   def add_post_by_title(new_title)
    a_post = Post.new(new_title)
